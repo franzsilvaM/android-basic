5a519ccbb45d7864f51b80715ac044bbbe6b8743
Franz Silva <franzsilvasky91@hotmail.com>
2015-06-18 10:26:36 -0500

    First Commit Hello World Project

---
+++ HelloWorld/app/src/main/java/com/franzsilva/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.franzsilva.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by FRANZ SILVA on 18/06/2015.
+ */
+public class MainActivity extends Activity {
+
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.main_activity);
+
+    }
+}
